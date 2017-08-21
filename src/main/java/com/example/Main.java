3f4240e210ab3f7c42203952b6d96595c6c792c1
jbilander <jorgen.bilander@creang.com>
2017-08-21 13:01:25 +0200

    Added Arial-font

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,12 +5,17 @@ import com.example.view.RootView;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
+        if (!(Util.isWindows() || Util.isMac())) { //Load Arial font for all *nix platforms
+            Font.loadFont(getClass().getResource(Util.FILE_SEPARATOR + "Arial.ttf").toExternalForm(), 12);
+        }
 
         RootView rootView = new RootView();
         Pane root = rootView.getView();
--- src/main/java/com/example/common/Util.java
+++ src/main/java/com/example/common/Util.java
@@ -5,6 +5,15 @@ public class Util {
     public static final int APP_HEIGHT = 400;
     public static final String TITLE = "JavaFx-structure-demo";
     public static final String USER_HOME = System.getProperty("user.home");
+    public static final String OS = System.getProperty("os.name").toLowerCase();
     public static final String FILE_SEPARATOR = System.getProperty("file.separator");
     public static final String JDBC_URL = "jdbc:sqlite:" + Util.USER_HOME + Util.FILE_SEPARATOR + "JavaFx-structure-demo.db";
+
+    public static boolean isWindows() {
+        return Util.OS.contains("win");
+    }
+
+    public static boolean isMac() {
+        return Util.OS.contains("mac") || Util.OS.contains("darwin");
+    }
 }
